3232014adce3e483238c13031cc15dc29105a722
takeshijmalau <takeshijicamalau@gmail.com>
2020-09-29 16:16:57 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat aliquam dolorem temporibus deserunt culpa accusantium dolor, consectetur possimus facere, soluta dolores explicabo molestias incidunt obcaecati animi ab aut autem illo?</p>
 </body>
 </html>
